fbd6bd3fc9ebb4366297ed30ee44bf2f944cfb2e
Michael Schennum and Ryan King <mikeschenphoto@gmail.com>
2016-03-23 10:02:33 -0700

    adds html selectors

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,53 @@
-import { Component } from 'angular2/core';
+import { Component, EventEmitter } from 'angular2/core';
+
+@Component({
+  selector: 'keg-display',
+  inputs: ['keg'],
+  template: `
+    <h3>{{ keg.name }}<li>{{ keg.brand }}<li>$ {{ keg.price }}<li>{{ keg.abv }}%</h3>
+  `
+})
+export class KegComponent {
+  public keg: Keg;
+}
+
+@Component({
+  selector: 'keg-list',
+  inputs: ['kegList'],
+  outputs: ['onKegSelect'],
+  directives: [KegComponent],
+  template: `
+  <keg-display *ngFor="#currentKeg of kegList"
+    (click)="kegClicked(currentKeg)"
+    [class.selected]="currentKeg === selectedKeg"
+    [keg]="currentKeg">
+  </keg-display>
+  `
+})
+export class KegListComponent {
+  public kegList: Keg[];
+  public onKegSelect: EventEmitter<Keg>;
+  public selectedKeg: Keg;
+  constructor() {
+    this.onKegSelect = new EventEmitter();
+  }
+  kegClicked(clickedKeg: Keg): void {
+    console.log('child', clickedKeg);
+    this.selectedKeg = clickedKeg;
+    this.onKegSelect.emit(clickedKeg);
+  }
+}
 
 @Component({
   selector: 'my-app',
+  directives: [KegListComponent],
   template: `
     <div class="container">
       <h1>Taproom App!</h1>
-      <ul>
-      <h3 *ngFor="#keg of kegs">{{ keg.name }}<h4><li>{{ keg.brand }}<li>$ {{ keg.price }}<li>{{ keg.abv }}%</h4></h3>
-      </ul>
+      <keg-list
+        [kegList]="kegs"
+        (onKegSelect)="kegWasSelected($event)">
+      </keg-list>
     </div>
   `
 })
@@ -21,6 +61,9 @@ export class AppComponent {
       new Keg("Lager", "Pabst", 2, 4, 2),
       new Keg("Pale Ale", "Sierra Nevada", 4, 5, 3)
     ];
+  }
+  kegWasSelected(clickedKeg: Keg): void {
+    console.log('parent', clickedKeg);
   }
 }
 
