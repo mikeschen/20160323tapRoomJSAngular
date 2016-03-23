6edfa340a3ee6d062f6bf000bba175b714e589b1
Michael Schennum and Ryan King <mikeschenphoto@gmail.com>
2016-03-23 14:54:42 -0700

    dcreases pints

--- app/keg.component.ts
+++ app/keg.component.ts
@@ -1,13 +1,17 @@
-import { Component } from 'angular2/core';
+import { Component, EventEmitter } from 'angular2/core';
 import { Keg } from './keg.model';
+import { KegListComponent  } from './keg-list.component';
 
 @Component({
   selector: 'keg-display',
   inputs: ['keg'],
   template: `
     <h3>{{ keg.name }}<li>{{ keg.brand }}<li>$ {{ keg.price }}<li>{{ keg.abv }}%<li>Pints Left: {{ keg.total }}</h3>
+    <button (click)="decreaseTotal(keg)">Sell Pint</button>
   `
 })
 export class KegComponent {
-  public keg: Keg;
+  decreaseTotal(keg) {
+    keg.total-=1;
+  }
 }
